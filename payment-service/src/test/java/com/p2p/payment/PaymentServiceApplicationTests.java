84d97636a54b6b9e2ddc723b231129ac5447e3ee
Evgeniy Kharin <evg.kharin@gmail.com>
2025-11-21 10:52:45 +0300

    feat: Add Spring Boot payment-service module

---
+++ payment-service/src/test/java/com/p2p/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2p.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
